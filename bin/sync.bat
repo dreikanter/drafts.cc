435fee379cb00157e92f1f11afa5a097d4d544fb
Alex Musayev <alex.musayev@gmail.com>
2014-05-12 16:27:28 +0400

    Theme update plus some PS changes follow up.

--- bin/sync.bat
+++
@@ -1,4 +0,0 @@
-set MEDIA_DIR=D:/dropbox/media.drafts.cc/
-aws s3 sync s3://media.drafts.cc %MEDIA_DIR% --exclude "*" --include "*.jpg" --include "*.png"
-aws s3 sync %MEDIA_DIR% s3://media.drafts.cc --exclude "*" --include "*.jpg" --content-type "image/jpeg"
-aws s3 sync %MEDIA_DIR% s3://media.drafts.cc --exclude "*" --include "*.png" --content-type "image/png"
